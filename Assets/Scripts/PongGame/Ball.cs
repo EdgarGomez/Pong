3cea7a4c4f6fac746b3a00429bdf70a47a476c46
Edgar J Gomez <edgar@ecomm.pro>
2023-01-14 01:21:48 +0100

    Handling winning and restart mechanics

--- Assets/Scripts/PongGame/Ball.cs
+++ Assets/Scripts/PongGame/Ball.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
@@ -9,13 +11,53 @@ public class Ball : MonoBehaviour
     private Rigidbody2D rb;
     public TextMeshProUGUI bluePlayerGoalsText;
     public TextMeshProUGUI redPlayerGoalsText;
-    int bluePlayerGoalsCounter = 1;
-    int redPlayerGoalsCounter = 1;
+    public TextMeshProUGUI winnerMessage;
+    public TextMeshProUGUI optionsMessage;
+    int bluePlayerGoalsCounter = 0;
+    int redPlayerGoalsCounter = 0;
+    int maxGoalsToWin = 2;
+    Color32 blueColor = new Color32(29, 67, 243, 255);
+    Color32 redColor = new Color32(204, 0, 0, 255);
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Invoke("GoBall", 1);
+    }
+
+    void Update()
+    {
+        if (bluePlayerGoalsCounter == maxGoalsToWin)
+        {
+            Winner("Blue");
+        }
+        if (redPlayerGoalsCounter == maxGoalsToWin)
+        {
+            Winner("Red");
+        }
+    }
+
+    void Winner(string color)
+    {
+        ResetBall();
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        winnerMessage.enabled = true;
+        optionsMessage.enabled = true;
+        if (color == "Blue")
+        {
+            winnerMessage.faceColor = blueColor;
+            winnerMessage.outlineColor = blueColor;
+        }
+        else
+        {
+            winnerMessage.faceColor = redColor;
+            winnerMessage.outlineColor = redColor;
+        }
+        winnerMessage.text = color + " Player Wins";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     void GoBall()
@@ -47,14 +89,16 @@ public class Ball : MonoBehaviour
     {
         if (other.CompareTag("LeftGoalLine"))
         {
-            bluePlayerGoalsText.text = (bluePlayerGoalsCounter++).ToString();
+            bluePlayerGoalsCounter = bluePlayerGoalsCounter + 1;
+            bluePlayerGoalsText.text = (bluePlayerGoalsCounter).ToString();
             ResetBall();
             RestartGame();
         }
 
         if (other.CompareTag("RightGoalLine"))
         {
-            redPlayerGoalsText.text = (redPlayerGoalsCounter++).ToString();
+            redPlayerGoalsCounter = redPlayerGoalsCounter + 1;
+            redPlayerGoalsText.text = (redPlayerGoalsCounter).ToString();
             ResetBall();
             RestartGame();
         }
